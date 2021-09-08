5bde796cd8938d71d01fc9655a2ffc42d4d6a128
Jackson Lauder <jacksonelauder@gmail.com>
2021-09-07 22:16:12 -0600

    dev stuff

---
+++ dev-server/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-path-concat */
+// eslint-disable-next-line no-unused-vars
+// const _ = require("lodash");
+// import express from "express";
+const express = require("express");
+// const history = require("connect-history-api-fallback");
+// import { registerRoutes } from './routes'
+// import { setEnvironment } from './config/env'
+// import { connectToDB } from './config/db'
+
+const app = express();
+// app.use("/", history());
+
+// setEnvironment(app)
+// connectToDB()
+// registerRoutes(app)
+
+// app.get('/', (req, res) => {
+//   if (process.env.NODE_ENV !== 'production') {
+//     return res.send('Running server in developer mode')
+//   } else {
+//     return res.sendFile('index.html', { root: __dirname + '/../dist/' })
+//   }
+// })
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(3000, () => {
+  console.log("Application listening on port 3000 in " + process.env.NODE_ENV);
+});
