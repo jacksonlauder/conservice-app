aaa844c9e526a86609c7d3d93a71af8bb4de9656
Jackson Lauder <jacksonelauder@gmail.com>
2021-09-13 17:19:56 -0600

    added reports and charts

---
+++ src/components/Chart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <canvas id="planet-chart"></canvas>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import Chart from "chart.js";
+import planetChartData from "../components/planet-data";
+
+export default {
+  name: "Chart",
+  data() {
+    return {
+      planetChartData: planetChartData,
+    };
+  },
+    mounted() {
+    const ctx = document.getElementById('planet-chart');
+    new Chart(ctx, this.planetChartData);
+  }
+};
+</script>
--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -5,5 +5,47 @@
         <h1 class="v-heading text-h4 text-sm-h4">Reports</h1>
       </v-col>
     </v-row>
+    <v-card>
+      <!-- <Chart /> -->
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        hide-default-header
+        hide-default-footer
+      ></v-data-table>
+    </v-card>
   </v-container>
 </template>
+
+<script>
+// import Chart from "../components/Chart.vue";
+export default {
+  name: "Reports",
+  components: {
+    // Chart
+  },
+
+  data: () => ({
+    headers: [
+      {
+        text: "name",
+        align: "start",
+        sortable: false,
+        value: "name",
+        width: "50%",
+      },
+      { text: "Number", value: "number", width: "50%" },
+    ],
+    items: [
+      {
+        name: "Weekly Hired",
+        number: 5,
+      },
+      {
+        name: "Terminated for current year",
+        number: 5,
+      },
+    ],
+  }),
+};
+</script>
