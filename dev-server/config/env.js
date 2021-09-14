37f0515d3ddc787660abb1ef7074bc9264646301
Jackson Lauder <jacksonelauder@gmail.com>
2021-09-14 15:17:50 -0600

    prod setup

--- dev-server/config/env.js
+++ dev-server/config/env.js
@@ -11,7 +11,6 @@ export function setEnvironment(app) {
 }
 
 function setDevEnv(app) {
-  console.log("Setting dev env");
   process.env.NODE_ENV = "development";
   process.env.DB_URL = "mongodb://localhost:27017/app-db";
   app.use(express.json());
@@ -20,7 +19,6 @@ function setDevEnv(app) {
 }
 
 function setProdEnv(app) {
-  console.log("Setting prod env");
   process.env.DB_URL = "mongodb://localhost:27017/prod-db";
   process.env.DB_URL = 'mongodb+srv://jacksonlauder:Bh39V8hHza0APaN5@conservice-app-db.otmd2.mongodb.net/conservice-app-db?retryWrites=true&w=majority'
   app.use(express.json());
