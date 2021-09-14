99c29c2c55f282892565a6647bbfd50cd8800f3f
Jackson Lauder <jacksonelauder@gmail.com>
2021-09-14 00:10:02 -0600

    Added Charts

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -5,47 +5,54 @@
         <h1 class="v-heading text-h4 text-sm-h4">Reports</h1>
       </v-col>
     </v-row>
-    <v-card>
-      <!-- <Chart /> -->
-      <v-data-table
-        :headers="headers"
-        :items="items"
-        hide-default-header
-        hide-default-footer
-      ></v-data-table>
-    </v-card>
+    <v-row justify="center">
+      <v-col cols="12" sm="4" lg="2">
+        <v-card>
+          <chart :chart-data="data" :options="options" />
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" lg="3">
+        <v-card>
+          <v-card-title>Weekly Hired</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text><h3>25</h3></v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="6" lg="3">
+        <v-card>
+          <v-card-title>Terminated for current year</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text><h3>25</h3></v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-// import Chart from "../components/Chart.vue";
+import Chart from "../components/Chart.js";
 export default {
   name: "Reports",
-  components: {
-    // Chart
-  },
+  components: { Chart },
 
   data: () => ({
-    headers: [
-      {
-        text: "name",
-        align: "start",
-        sortable: false,
-        value: "name",
-        width: "50%",
-      },
-      { text: "Number", value: "number", width: "50%" },
-    ],
-    items: [
-      {
-        name: "Weekly Hired",
-        number: 5,
-      },
-      {
-        name: "Terminated for current year",
-        number: 5,
-      },
-    ],
+    data: {
+      labels: ["Hired", "Terminated"],
+      datasets: [
+        {
+          label: "Number of Employees",
+          backgroundColor: ["#0D47A1", "#880E4F"],
+          data: [25, 25],
+        },
+      ],
+    },
+    options: {
+      legend: { display: true },
+      title: { display: false },
+    },
   }),
 };
 </script>
