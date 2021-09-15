8539e6c8bdd368af89c0dc8dd226eb377efccaff
Jackson Lauder <jacksonelauder@gmail.com>
2021-09-14 19:46:28 -0600

    prettify

--- dev-server/config/env.js
+++ dev-server/config/env.js
@@ -20,7 +20,8 @@ function setDevEnv(app) {
 
 function setProdEnv(app) {
   process.env.DB_URL = "mongodb://localhost:27017/prod-db";
-  process.env.DB_URL = 'mongodb+srv://jacksonlauder:Bh39V8hHza0APaN5@conservice-app-db.otmd2.mongodb.net/conservice-app-db?retryWrites=true&w=majority'
+  process.env.DB_URL =
+    "mongodb+srv://jacksonlauder:Bh39V8hHza0APaN5@conservice-app-db.otmd2.mongodb.net/conservice-app-db?retryWrites=true&w=majority";
   app.use(express.json());
   app.use(express.static(__dirname + "/../../dist"));
 }
