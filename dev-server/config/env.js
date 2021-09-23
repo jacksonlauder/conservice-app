45bc50fcddbd9fa16944202db87615e07b852fe7
Jackson Lauder <jacksonelauder@gmail.com>
2021-09-22 18:02:29 -0600

    debugging only

--- dev-server/config/env.js
+++ dev-server/config/env.js
@@ -12,7 +12,9 @@ export function setEnvironment(app) {
 
 function setDevEnv(app) {
   process.env.NODE_ENV = "development";
-  process.env.DB_URL = "mongodb://localhost:27017/app-db";
+  // process.env.DB_URL = "mongodb://localhost:27017/app-db";
+  process.env.DB_URL =
+    "mongodb+srv://jacksonlauder:Bh39V8hHza0APaN5@conservice-app-db.otmd2.mongodb.net/conservice-app-db?retryWrites=true&w=majority";
   app.use(express.json());
   app.use(morgan("dev"));
   app.use(cors());
